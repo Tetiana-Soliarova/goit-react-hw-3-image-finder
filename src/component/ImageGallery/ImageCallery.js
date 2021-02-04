dfc39a202f5a8a8b0162aae2eb7d16c27c09ffa4
Tetiana-Soliarova <t.s.skorpi1991@ukr.net>
2021-02-04 14:34:49 +0200

    deploy

--- src/component/ImageGallery/ImageCallery.js
+++ src/component/ImageGallery/ImageCallery.js
@@ -1,8 +1,5 @@
 import { Component } from 'react'
 
-
-
-
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem'
 import Button from '../Button/Button'
 import LoaderApp from '../Loader/loader'
@@ -19,16 +16,12 @@ export default class ImageCallery extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    //const nextPage = this.state.page
     if (prevProps.tagsName !== this.props.tagsName) {
       // console.log('изменилось имя ')
       // console.log('prevProps.tagsName:', prevProps.tagsName)
       //console.log('this.props.tagsName:', this.props.tagsName)
-      
       this.setState({ loading: true, images: [] })
       this.fetchImagesApi()
-
-      
     }
   }
 
@@ -39,7 +32,6 @@ export default class ImageCallery extends Component {
       `https://pixabay.com/api/?q=${this.props.tagsName}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
     )
       .then((response) => {
-         
         if (response.ok) {
           return response.json()
         }
@@ -48,12 +40,11 @@ export default class ImageCallery extends Component {
             `Картинки с даным названием ${this.state.tagsName}не найдено`,
           ),
         )
-       
       })
       .then((images) =>
         this.setState({ images: [...images.hits], page: this.state.page + 1 }),
-    )
-      
+      )
+
       .catch((error) => this.setState({ error }))
       .finally(() => this.setState({ loading: false }))
   }
@@ -64,7 +55,7 @@ export default class ImageCallery extends Component {
     fetch(
       `https://pixabay.com/api/?q=${this.props.tagsName}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
     )
-      .then((response) => { 
+      .then((response) => {
         if (response.ok) {
           return response.json()
         }
@@ -75,32 +66,29 @@ export default class ImageCallery extends Component {
         )
       })
 
-      .then((images) =>
-        this.setState((prevState) => ({
-          images: [...prevState.images, ...images.hits],
-          page: prevState.page + 1,
-        })),
-        this.onScrollTo()
-    ) //-
-      
-      
+      .then(
+        (images) =>
+          this.setState((prevState) => ({
+            images: [...prevState.images, ...images.hits],
+            page: prevState.page + 1,
+          })),
+        this.onScrollTo(),
+      ) //-
+
       .catch((error) => this.setState({ error }))
       .finally(() => this.setState({ loading: false }))
   }
 
-
- 
-
-onScrollTo() {
-    let value = document.body.scrollHeight;
+  onScrollTo() {
+    let value = document.body.scrollHeight
     setTimeout(() => {
-        window.scrollTo({
-            top: value,
-            left: 0,
-            behavior: 'smooth',
-        });
-    }, 1000);
-}
+      window.scrollTo({
+        top: value,
+        left: 0,
+        behavior: 'smooth',
+      })
+    }, 1000)
+  }
 
   //------------------MODAL---------------------
 
@@ -113,23 +101,17 @@ onScrollTo() {
     const { error, images, loading } = this.state
     return (
       <div>
-    
-
-    <ul className="ImageGallery">
+        <ul className="ImageGallery">
           {error && <h1>{error.message}</h1>}
 
           <ImageGalleryItem
-            images={this.state.images} key={this.state.images.id}
+            images={this.state.images}
+            key={this.state.images.id}
             onClick={this.toggleModalCloseOpen}
           />
+        </ul>
 
-
-          
-        </ul> 
-
-        
-
-         {loading && <LoaderApp />}
+        {loading && <LoaderApp />}
         {images.length > 0 && !loading && <Button onClick={this.fetchImages} />}
 
         {this.state.showModal && (
